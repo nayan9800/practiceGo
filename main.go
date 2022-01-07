0b69127016db4ab066a75f6749388da7e0e4a3bd
nayan9800 <nvsonawane3111@gmail.com>
2022-01-07 16:33:30 +0530

    Changes to be committed: 	modified:   main.go things that i learn: 	Data types and Type casting in Go 	    1. Go has data types int,uint,float,string 	       complex and rune 	    2. int, uint and float has 8,16,32,64             3. complex data types is used for complex 	       number operations 	    4. Rune datatype is used to save unicode value 	    5. In Go explict conversion id needed for Type 	       casting

--- main.go
+++ main.go
@@ -48,4 +48,73 @@ func main() {
 	fmt.Println(sub(10, 1))
 	fmt.Println(secToHrMinSec(3600))
 	fmt.Println(swap(1, 2))
+
+	/*2. Variables and types*/
+	/*Variables can be intialized using var keyword*/
+	var c, python, java bool
+	fmt.Println(c, python, java)
+	var hello string = "string"
+	fmt.Println(hello)
+
+	/*short variable declarations*/
+	a := 0
+	name := "hello"
+	fmt.Println(a, name)
+
+	/*Data types*/
+
+	//uint type have 8,16,32,64 variant
+	//default value is 0
+	var number uint32
+	number = 1<<32 - 1
+	fmt.Printf("%d has type %T\n", number, number)
+
+	//int type have 8,16,32,64 variant
+	//default value is 0
+	var number2 int32
+	number2 = 1554
+	fmt.Printf("%d has type %T\n", number2, number2)
+
+	//float
+	//has variant 32 and 64
+	//default value is 0.0
+	var f1 float64
+	f1 = 64.656442
+	fmt.Printf("%f has type %T\n", f1, f1)
+
+	//complex
+	//complex is data type  used for complex number operations
+	//complex data type has two varients complex128 and complex64
+	var comp1 complex64
+	comp1 = complex64(1 + 4i)
+	fmt.Printf("%v has type %T\n", comp1, comp1)
+
+	//string
+	//default value is ""
+	msg := "Hello my name is gopher"
+	fmt.Printf("%s has type %T\n", msg, msg)
+
+	//Rune
+	// Rune is datatype which is alias of uint32
+	//is used to store unicode values
+	//smiles is silce of the rune
+	smiles := []rune{'😃', '😎', '😈'}
+	/*here for loop is used to range over the slice of rune*/
+	for _, s := range smiles {
+		fmt.Printf("character:=%c, Unicode:=%U\n", s, s)
+	}
+
+	//constants
+	//constants are declared with const keyword
+	const pi = 3.14
+	fmt.Println(pi)
+
+	//Type Casting or type conversion
+	//in go explict conversion is needed to convert data type
+	var i int = 45
+	fmt.Printf("%d has type %T\n", i, i)
+	f2 := float32(i)
+	fmt.Printf("%f has type %T\n", f2, f2)
+	ui := uint(i)
+	fmt.Printf("%d has type %T\n", ui, ui)
 }
